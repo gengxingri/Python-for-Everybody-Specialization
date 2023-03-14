3b7dd04dd3bc0b05b99e00109417dd4bd93a9dd0
Bruce <54054499+gengxingri@users.noreply.github.com>
2023-03-14 15:52:52 +0800

    Update and rename ex_11.1.py to Assignment_11.1.py

---
+++ Using-Python-to-Access-Web-Data/Assignment_11.1.py
@@ -0,0 +1,17 @@
+# Assignment 11.1
+
+import re 
+digits = list()
+fh = open('regex_sum_1466060.txt')
+for line in fh:
+	line.rstrip()
+	num = re.findall('[0-9]+', line)
+	digits = digits+num
+length = len(digits)
+print(length)
+for i in range(length):
+	digits[i] = int(digits[i])
+print(sum(digits))
+   
+
+
