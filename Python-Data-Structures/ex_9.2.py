6998d695d053b74a04221476a4bbde7ff9faeae1
Bruce <54054499+gengxingri@users.noreply.github.com>
2023-03-14 14:43:03 +0800

    Add files via upload

---
+++ Python-Data-Structures/ex_9.2.py
@@ -0,0 +1,21 @@
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+handle = open(name)
+counts=dict()
+t_list=list()
+for line in handle:
+    if line.startswith('From '):
+        line.rstrip()
+        line=line.split()
+        time=line[5]
+        t_list.append(time[0:2])
+#sort time
+for t in t_list:
+        counts[t]=counts.get(t,0)+1
+list=sorted(counts.items())
+
+#print
+for r,v in list:
+    print(r,v)
+    
