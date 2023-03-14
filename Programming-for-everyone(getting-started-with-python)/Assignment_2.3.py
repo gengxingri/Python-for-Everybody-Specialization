9dddad12596bc587a64999231b449037d87bab58
Bruce <54054499+gengxingri@users.noreply.github.com>
2023-03-14 15:19:20 +0800

    Update Assignment_2.3.py

--- Programming-for-everyone(getting-started-with-python)/Assignment_2.3.py
+++ Programming-for-everyone(getting-started-with-python)/Assignment_2.3.py
@@ -1,6 +1,6 @@
 # Assignment 2.3
 
-xh=input('Enter hours:')
-xr=input('Enter rate:')
-xp=float(xh)*float(xr)
+xh = input('Enter hours:')
+xr = input('Enter rate:')
+xp = float(xh)*float(xr)
 print('Pay:', xp)
